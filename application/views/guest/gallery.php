9e1fe62175f5e91d6035d21562154b74b8241de7
jdcadenas <jdcadenas@gmail.com>
2018-05-05 17:16:19 -0400

    arreglos sabado 5 de mayo 02parte

--- application/views/guest/gallery.php
+++ application/views/guest/gallery.php
@@ -5,6 +5,53 @@
         <h3 class="w3ls-title">Nuestras fotos</h3>
         <div class="gallery-grids-top">
             <div class="gallery-grids">
+                <div class="col-md-4 col-sm-4 col-xs-6 gallery-grid-img">
+                    <a class="example-image-link w3-agilepic" href="<?= base_url('plantilla/images/image12.jpg') ?>" data-lightbox="example-set" data-title="">
+                        <img class="example-image img-responsive" src="<?= base_url('plantilla/images/image12.jpg') ?>" alt=""/>
+                        <div class="w3ls-overlay">
+                            <h4>Recepción</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-6 gallery-grid-img hover ehover14">
+                    <a class="example-image-link w3-agilepic" href="<?= base_url('plantilla/images/image2.jpg') ?>" data-lightbox="example-set" data-title="">
+                        <img class="example-image img-responsive" src="<?= base_url('plantilla/images/image2.jpg') ?>" alt=""/>
+                        <div class="w3ls-overlay">
+                            <h4></h4>
+                        </div>
+                    </a>
+                </div>
+
+
+                <div class="col-md-4 col-sm-4 col-xs-6 gallery-grid-img  hover ehover14">
+                    <a class="example-image-link w3-agilepic" href="<?= base_url('plantilla/images/image4.jpg') ?>" data-lightbox="example-set" data-title="">
+                        <img class="example-image img-responsive" src="<?= base_url('plantilla/images/image4.jpg') ?>" alt=""/>
+                        <div class="w3ls-overlay">
+                            <h4>Espacios</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="clearfix"> </div>
+                <div class="col-md-4 col-sm-4 col-xs-6 gallery-grid-img hover ehover14">
+                    <a class="example-image-link w3-agilepic" href="<?= base_url('plantilla/images/image5.jpg') ?>" data-lightbox="example-set" data-title="">
+                        <img class="example-image img-responsive" src="<?= base_url('plantilla/images/image5.jpg') ?>" alt=""/>
+                        <div class="w3ls-overlay">
+                            <h4>BabyGym</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-6 gallery-grid-img hover ehover14">
+                    <a class="example-image-link w3-agilepic" href="<?= base_url('plantilla/images/image3.jpg') ?>" data-lightbox="example-set" data-title="">
+                        <img class="example-image img-responsive" src="<?= base_url('plantilla/images/image3.jpg') ?>" alt=""/>
+                        <div class="w3ls-overlay">
+                            <h4></h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="clearfix"> </div>
+
+
+
                 <div class="col-md-6 col-sm-6 col-xs-6 gallery-grid-img  hover ehover14">
                     <a class="example-image-link w3-agilepic" href="<?= base_url('plantilla/images/psico1.jpg') ?>" data-lightbox="example-set" data-title="">
                         <img class="example-image img-responsive" src="<?= base_url('plantilla/images/psico1.jpg') ?>" alt=""/>
