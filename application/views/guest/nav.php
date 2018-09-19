26e57786e6fa91d7eeb4ab855a36f13786726440
jdcadenas <jdcadenas@gmail.com>
2018-09-19 05:41:09 -0400

    actualizacion septiembre 2018

--- application/views/guest/nav.php
+++ application/views/guest/nav.php
@@ -1,101 +1,101 @@
 <body>
-    <!-- header -->
-    <div class="header-w3layoutstop">
+  <!-- header -->
+  <div class="header-w3layoutstop">
+    <div class="container">
+      <div class="hdr-w3left navbar-left">
+        <p><span class="glyphicon glyphicon-earphone"></span>0414-3816504
+          <span class="glyphicon glyphicon-earphone"></span>0412-2452027 </p>
+      </div>
+      <div class="w3ls-bnr-icons social-icon navbar-right">
+
+
+        <a href="#" class="social-button google"><i class="fa fa-google-plus"></i> ciancentrointegral@gmail.com</a>
+        <a href="https://www.instagram.com/cian_centrointegral/" class="social-button instagram"><i class="fa fa-instagram"></i> Cian_CentroIntegral</a>
+      </div>
+    </div>
+  </div>
+  <!-- //header -->
+  <!-- banner -->
+  <div class="agileits-banner about-w3lsbnr">
+    <div class="bnr-agileinfo">
+      <!-- navigation -->
+      <div class="top-nav w3-agiletop">
         <div class="container">
-            <div class="hdr-w3left navbar-left">
-                <p><span class="glyphicon glyphicon-earphone"></span>0414-3816504
-                    <span class="glyphicon glyphicon-earphone"></span>0412-2452027 </p>
+          <div class="navbar-header ">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
+            <div class="w3menu navbar-right ">
+              <ul class="nav navbar " style="background-color: #9fa5a5">
+                <li class="nav-item "><a href="<?= site_url('home'); ?>" class="active">Inicio<span class="sr-only active">(current)</span></a></li>
+                <li class="nav-item"><a href="<?= site_url('home/about'); ?>">El Centro</a></li>
+                <li class="nav-item"><a href="<?= site_url('home/gallery'); ?>">Galería</a></li>
+                <!--
+
+                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                <span data-letters="Pages">Páginas</span><span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="<?= site_url('home/icons'); ?>">Iconos</a></li>
+                        <li><a href="<?= site_url('home/codes'); ?>">Códigos</a></li>
+                    </ul>
+                </li>
+
+                -->
+                <li class="nav-item"><a href="<?= site_url('home/contact'); ?>">Contacto</a></li>
+              </ul>
             </div>
-            <div class="w3ls-bnr-icons social-icon navbar-right">
+            <div class="clearfix"> </div>
+          </div>
+          <div class="container">
+            <div class="row w3llogo">
+              <div class="col-md-4 col-sm-4 col-xs-6 gallery-grid-img">
+                &nbsp;
+              </div>
+              <div class="col-md-4 col-sm-4 col-xs-6 gallery-grid-img">
+                <h1>
+                  <a href="<?= site_url('home'); ?>" >
+                    <img src="<?= site_url('../assets/img/logo.png'); ?>" class="img-responsive " />
+                  </a>
+                </h1>
+              </div>
+              <div class="col-md-4 col-sm-4 col-xs-6 gallery-grid-img">
+                &nbsp;
+              </div>
+            </div>
+
+          </div>
 
 
-                <a href="#" class="social-button google"><i class="fa fa-google-plus"></i> ciancentrointegral@gmail.com</a>
-                <a href="https://www.instagram.com/cian_centrointegral/" class="social-button instagram"><i class="fa fa-instagram"></i> Cian_CentroIntegral</a>
-            </div>
+
         </div>
-    </div>
-    <!-- //header -->
-    <!-- banner -->
-    <div class="agileits-banner about-w3lsbnr">
-        <div class="bnr-agileinfo">
-            <!-- navigation -->
-            <div class="top-nav w3-agiletop">
-                <div class="container">
-                    <div class="navbar-header ">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-
-                    </div>
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
-                        <div class="w3menu navbar-right ">
-                            <ul class="nav navbar " style="background-color: #9fa5a5">
-                                <li class="nav-item "><a href="<?= site_url('home'); ?>" class="active">Inicio<span class="sr-only active">(current)</span></a></a></li>
-                                <li class="nav-item"><a href="<?= site_url('home/about'); ?>">El Centro</a></li>
-                                <li class="nav-item"><a href="<?= site_url('home/gallery'); ?>">Galería</a></li>
-                                <!--
-
-                                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <span data-letters="Pages">Páginas</span><span class="caret"></span></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="<?= site_url('home/icons'); ?>">Iconos</a></li>
-                                        <li><a href="<?= site_url('home/codes'); ?>">Códigos</a></li>
-                                    </ul>
-                                </li>
-
-                                -->
-                                <li class="nav-item"><a href="<?= site_url('home/contact'); ?>">Contacto</a></li>
-                            </ul>
-                        </div>
-                        <div class="clearfix"> </div>
-                    </div>
-                    <div class="container">
-                        <div class="row w3llogo">
-                            <div class="col-md-4 col-sm-4 col-xs-6 gallery-grid-img">
-                                &nbsp;
-                            </div>
-                            <div class="col-md-4 col-sm-4 col-xs-6 gallery-grid-img">
-                                <h1>
-                                    <a href="<?= site_url('home'); ?>" >
-                                        <img src="<?= site_url('../assets/img/logo.png'); ?>" class="img-responsive " />
-                                    </a>
-                                </h1>
-                            </div>
-                            <div class="col-md-4 col-sm-4 col-xs-6 gallery-grid-img">
-                                &nbsp;
-                            </div>
-                        </div>
-
-                    </div>
 
 
+        <!-- Collect the nav links, forms, and other content for toggling -->
 
-                </div>
-
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-
-            </div>
+      </div>
 
 
-            <!-- //navigation -->
-            <div class="banner-w3text w3layouts">
-                <div class="container">
+      <!-- //navigation -->
+      <div class="banner-w3text w3layouts">
+        <div class="container">
 
-                    <h2 >Centro Integral de Ayuda al Niño</h2>
+          <h2 >Centro Integral de Ayuda al Niño</h2>
 
-                    <div class="hidden">
-                        <a class="btn btn-primary btn-lg btn-primary-outline hidden" href="<?= site_url('appointments'); ?>">
-                            <i class="fa fa-book" ></i>
-                            <span >Hacer cita</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <!-- //hacer cita fin -->
+          <div class="hidden">
+            <a class="btn btn-primary btn-lg btn-primary-outline hidden" href="<?= site_url('appointments'); ?>">
+              <i class="fa fa-book" ></i>
+              <span >Hacer cita</span>
+            </a>
+          </div>
         </div>
+      </div>
+      <!-- //hacer cita fin -->
     </div>
+  </div>
